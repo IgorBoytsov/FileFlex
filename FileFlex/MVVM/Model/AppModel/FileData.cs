aea0021cd1e2f5f984f4a4a2f71fe6b03aa72b00
Light Play <lightplay456@gmail.com>
2024-11-08 21:02:54 +0300

    Создан класс-модель для представления выбранных файлов в программе.

---
+++ FileFlex/MVVM/Model/AppModel/FileData.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace FileFlex.MVVM.Model.AppModel
+{
+    public class FileData
+    {
+        public string FileName { get; set; }
+
+        public string FileExtension { get; set; }
+        
+        public BitmapImage FileIcon { get; set; }
+
+        public string FilePath { get; set; }
+
+        public double FileWeight { get; set; }
+
+        public DateTime DateCreate { get; set; }
+    }
+}
