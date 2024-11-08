b098d67ae087481e85f3b06da223112a296dee22
Light Play <lightplay456@gmail.com>
2024-11-08 21:08:25 +0300

    Добавлено перечисление для выбора действий с файлами.

---
+++ FileFlex/Utils/Enums/FileAction.cs
@@ -0,0 +1,12 @@
+﻿namespace FileFlex.Utils.Enums
+{
+    enum FileAction
+    {
+        Open = 0,
+        Add = 1,
+        Update = 2,
+        Remove = 3,
+        Delete = 4,
+        MoveToTrash = 5,
+    }
+}
