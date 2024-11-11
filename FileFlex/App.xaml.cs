d94258a95af4706b7b2838578941fb54d62dc600
Light Play <lightplay456@gmail.com>
2024-11-11 12:14:26 +0300

    Настроент Dependency injection.

--- FileFlex/App.xaml.cs
+++ FileFlex/App.xaml.cs
@@ -2,6 +2,8 @@
 using FileFlex.MVVM.ViewModels.WindowViewModels;
 using FileFlex.Utils.Services;
 using FileFlex.Utils.Services.FileConvertServices;
+using FileFlex.Utils.Services.FileDialogServices;
+using FileFlex.Utils.Services.NavigationServices;
 using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
@@ -28,14 +30,30 @@ namespace FileFlex
             base.OnStartup(e);
         }
 
+        //  AddTransient - 
+        //    Описание: Каждый раз, когда вы запрашиваете сервис, создается новый экземпляр.
+        //    Когда использовать: Для легковесных и независимых сервисов, где создание нового объекта не требует значительных ресурсов.
+        //  AddScoped
+        //      Описание: Экземпляр создается один раз на каждый запрос.Это означает, что в пределах одного HTTP-запроса(например, в ASP.NET Core) вы получите один и тот же экземпляр, но для разных запросов будут создаваться разные экземпляры.
+        //      Когда использовать: Когда необходимо, чтобы сервис разделялся между компонентами в рамках одного запроса, но не между разными запросами.
+        //  AddSingleton
+        //      Описание: Экземпляр создается один раз и используется на протяжении всего времени работы приложения.Все запросы к сервису будут получать один и тот же экземпляр.
+        //      Когда использовать: Для тяжелых или долговечных объектов, которые должны сохранять состояние и инициализируются один раз.
+        
         private static void ConfigureServices(ServiceCollection services)
         {
-            services.AddTransient<IFileConvertService, ImageConvertService>();
-            services.AddTransient<IFileConvertService, DocumentConvertService>();
-            services.AddTransient<IFileConvertService, AudioConvertService>();
-
             services.AddSingleton<MainWindowViewModel>();
             services.AddSingleton<MainWindow>();
+
+            services.AddSingleton<IFileConvertService, ImageConvertService>();
+            services.AddSingleton<IFileConvertService, DocumentConvertService>();
+
+            services.AddSingleton<INavigationService, PageNavigationService>();
+            services.AddSingleton<INavigationService, WindowNavigationService>();
+
+            services.AddSingleton<IFileDialogService, OpenFileDialogService>();
+            services.AddSingleton<IFileDialogService, SaveFileDialogService>();
+           
         }
     }
 }
