26e4e49c7e171dcb516f90264c9a963da3789467
Light Play <lightplay456@gmail.com>
2025-01-03 13:49:17 +0300

    Конвертер для отсутствия отображение типа файлов у папок в списке файлов.

---
+++ FileFlex/Utils/ValueConverters/FileExtensionFolderToStringEmptyConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace FileFlex.Utils.ValueConverters
+{
+    class FileExtensionFolderToStringEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string FileExtension)
+            {
+                if (FileExtension == "Folder")
+                {
+                    return "";
+                }
+                else
+                {
+                    return FileExtension;
+                }
+            }
+            return "N/A FileExtension";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
