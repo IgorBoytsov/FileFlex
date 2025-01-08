16d952397d856f7a1278945b045d7fad809161b5
Light Play <lightplay456@gmail.com>
2025-01-08 16:23:54 +0300

    Создан класс FolderHelper.

---
+++ FileFlex/Utils/Helpers/FolderHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+namespace FileFlex.Utils.Helpers
+{
+    public static class FolderHelper
+    {
+        private static async Task<long> GetFolderSize(string folderPath)
+        {
+            return await Task.Run(() =>
+            {
+                long totalSize = 0;
+
+                try
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
+                    FileInfo[] files = directoryInfo.GetFiles();
+
+                    foreach (FileInfo file in files)
+                    {
+                        totalSize += file.Length;
+                    }
+
+                    DirectoryInfo[] subDirectories = directoryInfo.GetDirectories();
+                    foreach (DirectoryInfo subDirectory in subDirectories)
+                    {
+                        totalSize += GetFolderSize(subDirectory.FullName).Result;
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+
+                }
+                catch (Exception)
+                {
+
+                }
+
+                return totalSize;
+            });
+        }
+    }
+}
